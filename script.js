4ae277a0acf5b4ef1ab7c1b9647527784dd8cb64
MuhseenSD <abdulmuhsinsani360@gmail.com>
2026-06-22 11:53:31 +0100

    Add error handling for invalid coins and netwrok failure

--- script.js
+++ script.js
@@ -18,16 +18,33 @@ function fetchCoinPrice(coinId){
     fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=usd`)
     .then(response => response.json())
     .then(data => {
+        // Error handling 
+        if(data[coinId] === undefined){
+            let errMsg = document.createElement("p")
+            errMsg.textContent = `${coinId}: coin not found`
+            document.getElementById("watchlist").appendChild(errMsg)
+
+            return
+        }
+
         console.log(data[coinId].usd)
-        
+                
         let newEl = document.createElement("p")
         
         newEl.textContent = `${coinId} price: ${data[coinId].usd}`
 
         document.getElementById("watchlist").appendChild(newEl)
+
+    })
+
+    .catch(error => {
+        let errMsg2 = document.createElement("p")
+        errMsg2.textContent = `Something went wrong`
+        document.getElementById("watchlist").appendChild(errMsg2)
     })
 }
 
 fetchCoinPrice("bitcoin")
 fetchCoinPrice("ethereum")
+fetchCoinPrice("fakecoin123")
 
