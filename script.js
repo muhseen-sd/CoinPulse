b2a66b4e06c625f13fde74ad40227a68f0a22470
MuhseenSD <abdulmuhsinsani360@gmail.com>
2026-06-22 11:03:14 +0100

    Add reusable fetchCoinPrice function, display multiple coins dynamically

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 console.log("1: starting up");
 
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-  .then(response => response.json())
-  .then(data => {
-    console.log("3: got price data", data.bitcoin.usd)
+// fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log("3: got price data", data.bitcoin.usd)
 
-    let btcPrice = document.getElementById("btc-price")
-    btcPrice.innerHTML = data.bitcoin.usd
+//     let btcPrice = document.getElementById("btc-price")
+//     btcPrice.innerHTML = data.bitcoin.usd
 
-});
+// });
+
+// fetchCoinPrice("bitcoin")
 
 console.log("2: script keeps running"); 
+
+function fetchCoinPrice(coinId){
+    fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coinId}&vs_currencies=usd`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[coinId].usd)
+        
+        let newEl = document.createElement("p")
+        
+        newEl.textContent = `${coinId} price: ${data[coinId].usd}`
+
+        document.getElementById("watchlist").appendChild(newEl)
+    })
+}
+
+fetchCoinPrice("bitcoin")
+fetchCoinPrice("ethereum")
 
